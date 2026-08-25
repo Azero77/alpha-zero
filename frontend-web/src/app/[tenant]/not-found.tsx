34288217c35c06cd521ae81b8541b4c186b361d7
Azero77 <zaggar.azero.m@gmail.com>
2026-08-25 15:52:19 +0300

    will start frontend again with react using bullet proof architecture

--- frontend-web/src/app/[tenant]/not-found.tsx
+++
@@ -1,7 +0,0 @@
-export default function NotFound() {
-  return (
-    <div className="flex h-screen w-full items-center justify-center">
-      <h1 className="text-2xl font-bold">Tenant Not Found</h1>
-    </div>
-  );
-}
